1e340fcfc6409f309dea8056f99f80b04c45412d
Victor <victormakanda254@gmail.com>
2024-01-21 14:21:39 +0300

    Set routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Layout from './layout/Layout';
+import Home from './pages/Home';
+import Restaurant from './pages/Restaurant';
 
 function App() {
   return (
     <div>
+      <BrowserRouter >
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/restaurants/:id" element={<Restaurant />}/>
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
      
     </div>
   );
